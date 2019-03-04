760bbd31863da9500fb8c9ac84c24c0b1a115f0e
Markiz <hpb0hpb@gmail.com>
2019-03-04 14:08:33 +0300

    add Tree

---
+++ AVL/Tree.h
@@ -0,0 +1,27 @@
+#pragma once
+
+struct node // структура для представления узлов дерева
+{
+	int key;
+	unsigned char height;
+	node* left;
+	node* right;
+	node(int k) { key = k; left = right = 0; height = 1; }
+};
+
+class AVLTree
+{
+private:
+	unsigned char height(node* p);
+	int bfactor(node* p);
+	void Fixheight(node* p);
+	node* rotateright(node* p); // правый поворот вокруг p
+	node* rotateleft(node* q); // левый поворот вокруг q
+	node* balance(node* p); // балансировка узла p
+	node* findmin(node* p); // поиск узла с минимальным ключом в дереве 
+	node* removemin(node* p); // удаление узла с минимальным ключом из дерева p
+
+public:
+	node* insert(node* p, int k); // вставка ключа k в дерево с корнем p
+	node* remove(node* p, int k); // удаление ключа k из дерева p
+};
